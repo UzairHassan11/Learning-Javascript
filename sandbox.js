c2982473d111124eccc4d68100fea1ad99de42d2
mac <uzairhassan11@gmail.com>
2024-06-11 10:45:11 +0500

    add function and method difference

--- sandbox.js
+++ sandbox.js
@@ -1,34 +1,11 @@
-// function expression
-// const sayHi = function(){
-//     console.log("Hello world");
-// };
 
-// sayHiTo();
+const name = 'Ali';
 
-// function sayHiTo(name = 'Ahmad', time = 5){
-//     console.log(`Hello, ${name}. It's ${time} O'Clock.`);
-// }
+// function 
 
-// const calcArea = function(radius){
-//     let area = 3.14 * radius**2;
-//     return area;
-// }
+const sayHi = () => 'Hello world';
+const greet = sayHi();
+console.log(greet);
 
-// const calcRectangleArea = (length, width) => length * width;
-
-// let area = calcRectangleArea(2, 3);
-// console.log("area of the rectangle is " + area);
-
-// const sayHi = () => 'Hello, world.';
-// console.log(sayHi());
-
-const calcBill = (productPrices, tax) => {
-    let bill = 0;
-    for(let i = 0; i < productPrices.length; i++)
-    {
-        bill += productPrices[i] + productPrices[i]*tax;
-    }
-    return bill;
-};
-const bill = calcBill([10, 20, 30], .2);
-console.log(bill);
+// method
+console.log(name.toUpperCase());
