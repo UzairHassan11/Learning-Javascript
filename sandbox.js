adc714936ab5c299d46136b3f9223de1bddd00c3
mac <uzairhassan11@gmail.com>
2024-06-11 10:20:48 +0500

    return value from func

--- sandbox.js
+++ sandbox.js
@@ -3,8 +3,16 @@
 //     console.log("Hello world");
 // };
 
-sayHiTo();
+// sayHiTo();
 
-function sayHiTo(name = 'Ahmad', time = 5){
-    console.log(`Hello, ${name}. It's ${time} O'Clock.`);
+// function sayHiTo(name = 'Ahmad', time = 5){
+//     console.log(`Hello, ${name}. It's ${time} O'Clock.`);
+// }
+
+const calcArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
 }
+
+let area = calcArea(2);
+console.log("area of the circle is " + area);
