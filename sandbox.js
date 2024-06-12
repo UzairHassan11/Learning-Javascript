6d8c5f1933a3cc0e21bc2b666493a3c9a2ed7ea8
mac <uzairhassan11@gmail.com>
2024-06-12 16:07:59 +0500

    clear scripts

--- sandbox.js
+++
@@ -1,9 +0,0 @@
-let people = ['Ali', 'Hassan', 'Akram', 'Moeen', 'Waqar'];
-const ul = document.querySelector(".persons");
-
-let html = ``;
-
-people.forEach((person) => html+=`<li style = "color: blue">${person}</li>`);
-
-// console.log(html);
-ul.innerHTML = html;
