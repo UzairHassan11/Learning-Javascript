f87a3146deb917e1ee136c7b13c5d0482843fd3d
mac <uzairhassan11@gmail.com>
2024-06-12 16:06:02 +0500

    primitive type vs reference type

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,14 @@
-console.log(Math);
-console.log(Math.PI);
+let num1 = 50;
+let num2 = 50;
 
-let num = 14.9;
-console.log(Math.ceil(num));
-console.log(Math.floor(num));
-console.log(Math.round(num));
-console.log(Math.trunc(num));
+console.log(`first number ${num1} , second number ${num2}`);
+num2 = 100;
+console.log(`first number ${num1} , second number ${num2}`);
 
-console.log(Math.random());
-console.log(Math.random() * 100);
+const user1 = {name: 'Ali', gender: 'Male',age: 30};
+console.log(user1);
+const user2 = user1;
+console.log(user2);
+user2.age = 40;
+console.log(user1);
+console.log(user2);
