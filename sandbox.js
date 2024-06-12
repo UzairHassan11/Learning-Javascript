0c8cfc96cb03d5fd8f2a8c04856bc8704ba4ce1f
mac <uzairhassan11@gmail.com>
2024-06-12 15:19:36 +0500

    Add Math object and it's methods

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,11 @@
-// const blogs = [
-//     {title: 'Thesis', likes: 20},
-//     {title: 'Minions', likes: 30}
-// ]
+console.log(Math);
+console.log(Math.PI);
 
-let user = {
-    name: 'Ali',
-    age: 30,
-    email: 'ali@gmail.com',
-    location: 'Lahore',
-    blogs: [
-        {title: 'Thesis', likes: 20},
-        {title: 'Minions', likes: 30}
-    ],
-    login(){
-        console.log('this is login function');
-    },
-    logAge: ()=> {
-        this.age = 19;
-        console.log(this.age);
-    },
-    logBlogs() {
-        console.log(`blogs written by ${this.name}`);
-        this.blogs.forEach(blog => {
-            console.log(`The blog named ${blog.title} has ${blog.likes} likes.`);
-        });
-    }
-};
+let num = 14.9;
+console.log(Math.ceil(num));
+console.log(Math.floor(num));
+console.log(Math.round(num));
+console.log(Math.trunc(num));
 
-user.login();
-user.logAge();
-user.logBlogs();
+console.log(Math.random());
+console.log(Math.random() * 100);
