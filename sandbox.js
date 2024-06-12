33a9cc8400d4684f2fe5b5227fd13c09857813f1
mac <uzairhassan11@gmail.com>
2024-06-12 17:33:09 +0500

    use querySelectorAll
    to get all the elements with the specific query

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,21 @@
-let para = document.querySelector('p');
-console.log(para);
+// let para = document.querySelector('p');
+// console.log(para);
 
-para = document.querySelector('.error');
-console.log(para);
+// para = document.querySelector('.error');
+// console.log(para);
 
-para = document.querySelector('.error');
-console.log(para);
+// para = document.querySelector('.error');
+// console.log(para);
 
-para = document.querySelector('body > h1');
-console.log(para);
+// para = document.querySelector('body > h1');
+// console.log(para);
 
+const paras = document.querySelectorAll('p');
+console.log(paras);
+
+paras.forEach(para =>{
+    console.log(para);
+});
+
+const errors = document.querySelectorAll('.error');
+console.log(errors);
