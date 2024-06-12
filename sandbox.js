195c3fc4712c487a7a4e22105d7abc682919aaac
mac <uzairhassan11@gmail.com>
2024-06-12 16:55:16 +0500

    get elements of html page
    using document keywords and querySelector you grab any element of the web page

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+let para = document.querySelector('p');
+console.log(para);
+
+para = document.querySelector('.error');
+console.log(para);
+
+para = document.querySelector('.error');
+console.log(para);
+
+para = document.querySelector('body > h1');
+console.log(para);
+
